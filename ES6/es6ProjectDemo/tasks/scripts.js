2c903145c13a41f05729807184a6cc6366b2fd66
hongwanzhen <hongwanzhen@cmss.chinamobile.com>
2020-02-29 00:42:45 +0800

    my first es6 project

---
+++ ES6/es6ProjectDemo/tasks/scripts.js
@@ -0,0 +1,7 @@
+import gulp from 'gulp';
+import gulpif from 'gulp-if';
+import concat from 'gulp-concat';
+import webpack from 'webpack';
+import gulpWebpack from 'webpack-stram';
+import named from 'vinyl-named';
+
